ce3d18642b7fbc11965c2eac80c7745da9e5a0fb
Joris Seyadou <seyadou.joris@gmail.com>
2023-04-23 12:41:07 +0200

    test

---
+++ api-src/serverless.ts
@@ -0,0 +1,13 @@
+import cors from "@fastify/cors";
+import Fastify from "fastify";
+
+const app = Fastify({
+  logger: true,
+});
+
+app.register(cors).register(import("@functions/index"));
+
+export default async (req: any, res: any) => {
+  await app.ready();
+  app.server.emit("request", req, res);
+};
